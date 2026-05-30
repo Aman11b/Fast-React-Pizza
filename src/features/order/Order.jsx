639b825b774496f10bfa15beed1e8b00daabf833
Aman11b <amansingh1100b@gmail.com>
2026-05-30 21:28:01 +0530

    A new way to implement Routers

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -4,8 +4,7 @@ import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
-} from "../../utils/helpers";
-
+} from "../../utilities/helpers";
 const order = {
   id: "ABCDEF",
   customer: "Jonas",
@@ -44,13 +43,13 @@ const order = {
 function Order() {
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
-    id,
+    // id,
     status,
     priority,
     priorityPrice,
     orderPrice,
     estimatedDelivery,
-    cart,
+    // cart,
   } = order;
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
