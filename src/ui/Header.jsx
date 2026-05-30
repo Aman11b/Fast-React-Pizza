f6bb4804492aea56eb22bcdc0b7cfb2d3c8aa863
Aman11b <amansingh1100b@gmail.com>
2026-05-30 22:22:36 +0530

    Building App Layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <p>Jhones</p>
+    </header>
+  );
+}
+
+export default Header;
