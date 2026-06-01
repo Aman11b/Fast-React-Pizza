5edfdb6d7e02091eafa69e4d276c5e83cbb57523
Aman11b <amansingh1100b@gmail.com>
2026-06-01 10:37:57 +0530

    setting up tailwind css

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -4,8 +4,8 @@ function CartOverview() {
   return (
     <div>
       <p>
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>23 pizzas </span>
+        <span>$23.45 </span>
       </p>
 
       <Link to="/cart">Open cart</Link>
