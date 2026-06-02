03904ac2d25b9c19d12c39fa5f899d8cba2ecc0a
Aman Singh <amansingh1100b@gmail.com>
2026-06-02 18:24:25 +0000

    Updating cart quantities

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -36,7 +36,9 @@ const cartSlice=createSlice({
              //payload=id
              const item=state.cart.find((item)=>item.pizzaId===action.payload);
              item.quantity--;
-             item.totalPrice=item.quantity*item.unitPrice
+             item.totalPrice=item.quantity*item.unitPrice;
+
+             if(item.quantity === 0)  cartSlice.caseReducers.deleteItem(state,action);
         },
         clearCart(state){
             state.cart=[]
